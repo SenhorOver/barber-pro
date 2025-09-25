d607269dc779cfef5d7a0be2f4a40557e02b8801
Marcos V Silva <mrcsvs@outlook.com>
2025-09-24 21:07:47 -0300

    feat(chakraui): add chakraui and configure

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>BarberPro - Seu sistema completo</title>
+      </Head>
       <div>
         <h1>Hello World!</h1>
       </div>
