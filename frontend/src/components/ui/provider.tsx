75256fd9b8f1bea7c1b90a053225c0d3985c1ab0
Marcos V Silva <mrcsvs@outlook.com>
2025-09-25 18:41:58 -0300

    refactor(chakra): change custom styles to provider component

--- frontend/src/components/ui/provider.tsx
+++ frontend/src/components/ui/provider.tsx
@@ -1,15 +1,45 @@
-"use client"
+"use client";
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
+  ChakraProvider,
+  createSystem,
+  defaultConfig,
+  defineConfig,
+} from "@chakra-ui/react";
+
+import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
+
+const colors = {
+  barber: {
+    900: { value: "#12131b" },
+    400: { value: "#1b1c29" },
+    100: { value: "#c6c6c6" },
+  },
+  button: {
+    cta: { value: "#fba931" },
+    DEFAULT: { value: "#fff" },
+    gray: { value: "#dfdfdf" },
+    danger: { value: "#ff4040" },
+  },
+  orange: {
+    900: { value: "#fba931" },
+  },
+};
+
+const config = defineConfig({
+  theme: {
+    tokens: {
+      colors,
+    },
+  },
+});
+
+const system = createSystem(defaultConfig, config);
 
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
+    <ChakraProvider value={system}>
+      <ColorModeProvider {...props} defaultTheme="dark" />
     </ChakraProvider>
-  )
+  );
 }
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,45 +1,10 @@
 import type { AppProps } from "next/app";
-import {
-  ChakraProvider,
-  createSystem,
-  defaultConfig,
-  defineConfig,
-} from "@chakra-ui/react";
-import { ColorModeProvider } from "@/components/ui/color-mode";
-
-const colors = {
-  barber: {
-    900: { value: "#12131b" },
-    400: { value: "#1b1c29" },
-    100: { value: "#c6c6c6" },
-  },
-  button: {
-    cta: { value: "#fba931" },
-    DEFAULT: { value: "#fff" },
-    gray: { value: "#dfdfdf" },
-    danger: { value: "#ff4040" },
-  },
-  orange: {
-    900: { value: "#fba931" },
-  },
-};
-
-const config = defineConfig({
-  theme: {
-    tokens: {
-      colors,
-    },
-  },
-});
-
-const system = createSystem(defaultConfig, config);
+import { Provider } from "@/components/ui/provider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider value={system}>
-      <ColorModeProvider defaultTheme="dark">
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
   );
 }
